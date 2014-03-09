b61fcf1787cb269abb4fbcfce2ddf33a1b3ea717
ajayaddula <aaf44@mail.umkc.edu>
2014-03-08 17:52:44 -0800

    LAB 4

---
+++ SOAP/App_Code/WebService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+/// <summary>
+/// Summary description for WebService
+/// </summary>
+[WebService(Namespace = "http://tempuri.org/")]
+[WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+// To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+// [System.Web.Script.Services.ScriptService]
+public class WebService : System.Web.Services.WebService {
+
+    public WebService () {
+
+        //Uncomment the following line if using designed components 
+        //InitializeComponent(); 
+    }
+
+    [WebMethod]
+    public string Decimal(int d) 
+    {
+        string binary = Convert.ToString(d, 2);
+        return binary;
+    }
+
+    [WebMethod]
+    public long binary(string b)
+    {
+        long Decimal = Convert.ToInt64(b, 2);
+        return Decimal;
+    }
+}
